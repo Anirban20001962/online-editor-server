ff42a86a984d079950207b42f25dbdb7b6c427f0
RanjanKumar666 <typhonazazel@gmail.com>
2021-09-03 11:39:47 +0530

    Updated the ts-node

--- server/src/socket/handler.ts
+++ server/src/socket/handler.ts
@@ -50,8 +50,8 @@ export default function handler(socket: Socket) {
         if (language === LANGUAGES.TS) {
             fileRunner({
                 ...runOptions,
-                cmd: "npx",
-                args: ["ts-node", filename],
+                cmd: "ts-node",
+                args: [filename],
             });
         }
     });
